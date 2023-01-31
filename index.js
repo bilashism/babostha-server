4841268c30ad24fb09b5654e296d33dd19fcddf5
Md. Taifuzzaman Bilash <bilashism@gmail.com>
2023-01-31 18:45:28 +0600

    setting up mongoose

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 import { config } from "dotenv";
 import express, { json } from "express";
 import cors from "cors";
+import mongoose from "mongoose";
 
 config();
 const app = express();
@@ -9,6 +10,10 @@ const port = process.env.PORT || 5000;
 // using middleware
 app.use(cors());
 app.use(json());
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PWD}@${process.env.DB_CLUSTER_URL}/?retryWrites=true&w=majority`;
+mongoose.set("strictQuery", true);
+mongoose.connect(uri).catch(console.error);
 
 // localhost server setup
 const server = app.listen(port, "localhost", () => {
